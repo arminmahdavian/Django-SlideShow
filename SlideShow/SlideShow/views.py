72b9227dd04748e511f8a5f682b375d9152125ec
arminmahdavian <armin.mail007@gmail.com>
2024-07-27 15:44:49 +0100

    completed

--- SlideShow/SlideShow/views.py
+++ SlideShow/SlideShow/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from .models import SlideShow
+
 
 
 # Create your views here.
 
 
 def home(request):
-    context = {
-        "slides": SlideShow.objects.filter(status=True, article__status=True),
-    }
-    return render(request, 'SlideShow/home.html', context)
+    return render(request, 'SlideShow/home.html')
 
